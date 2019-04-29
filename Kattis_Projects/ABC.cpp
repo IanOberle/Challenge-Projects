e71ae6ad6806626db486b5ae7efad3b7a0282075
Ian Oberle <48143183+IanOberle@users.noreply.github.com>
2019-04-29 15:19:21 -0500

    Update ABC.cpp

--- Kattis_Projects/ABC.cpp
+++ Kattis_Projects/ABC.cpp
@@ -1,3 +1,18 @@
+/* Kattis Project Description
+You will be given three integers A, B and C. The numbers will not be given in that exact order, 
+but we do know that A is less than B and B less than C. In order to make for a more pleasant viewing, 
+we want to rearrange them in a given order.
+
+Input
+The first line contains the three positive integers A, B and C, not necessarily in that order. 
+The three numbers will be less than or equal to 100. The second line contains three uppercase letters 
+’A’, ’B’ and ’C’ (with no spaces between them) representing the desired order.
+
+Output
+Output A, B and C in the desired order on a single line, separated by single spaces.
+*/
+
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,15 +25,19 @@ int main()
 	int holder1 = 0, holder2 = 0, middle = 0, a, b, c;
 	string letters;
 
+	//Enter in numbers A-C and enter a display order
 	cin >> a >> b >> c;
 	arr.push_back(a);
 	arr.push_back(b);
 	arr.push_back(c);
 	cin >> letters;
+	
 	for (int i = 0; i < 3; i++)
 	{
 		letters[i] = toupper(letters[i]);
 	}
+	
+	//Searching for the highest number "C" and the lowest number "A"
 	int highest = arr[0];
 	int lowest = arr[0];
 	for (int i = 0; i < 3; i++)
@@ -35,12 +54,14 @@ int main()
 		}
 	}
 	
+	//Determine which value is "B"
 	for (int i = 0; i < 3; i++)
 	{
 		if (i != holder1 && i != holder2)
 			middle = i;
 	}
 	
+	//Display numbers in the order specified
 	for (int i = 0; i < 3; i++)
 	{
 		if (letters[i] == 'C')
