6460681c1e8c5815b243d93b975b5a9351199cc8
Ian Oberle <48143183+IanOberle@users.noreply.github.com>
2019-04-29 17:02:42 -0500

    Update Autori.cpp

--- Kattis_Projects/Autori.cpp
+++ Kattis_Projects/Autori.cpp
@@ -1,5 +1,19 @@
 Autori
 
+/*Kattis Project Description
+Great scientific discoveries are often named by the last names of scientists that made them. For example, the most popular asymmetric cryptography system, RSA was discovered by Rivest, Shamir and Adleman. Another notable example is the Knuth-Morris-Pratt algorithm, named by Knuth, Morris and Pratt.
+
+Scientific papers reference earlier works a lot and it’s not uncommon for one document to use two different naming conventions: the short variation (e.g. KMP) using only the first letters of authors last names and the long variation (e.g. Knuth-Morris-Pratt) using complete last names separated by hyphens.
+
+We find mixing two conventions in one paper to be aesthetically unpleasing and would like you to write a program that will transform long variations into short.
+
+Input
+The first and only line of input will contain at most 100 characters, uppercase and lowercase letters of the English alphabet and hyphen (‘-’ ASCII 45). The first character will always be an uppercase letter. Hyphens will always be followed by an uppercase letter. All other characters will be lowercase letters.
+
+Output
+The first and only line of output should contain the appropriate short variation.
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -9,7 +23,10 @@ int main()
 {
 	string names, initials;
 
+	//Enter names
 	cin >> names;
+	
+	//Find capital letters (initials)
 	for (int i = 0; i < names.length() - 1; i++)
 	{
 		if (isupper(names[i]))
@@ -18,8 +35,8 @@ int main()
 		}
 	}
 	
+	//Print out the initials
 	cout << initials << endl;
 
-	//system("pause");
 	return 0;
 }
